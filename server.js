b5da476f5e20c7fd175bb52b61f357a07acb55c3
Glen Quirós Ortiz <glenquor@gmail.com>
2022-01-19 22:40:10 -0600

    first version

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
+const { json } = require('express');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -23,6 +24,34 @@ app.get("/", function (req, res) {
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
+
+// if the user request this route send to the user the current timestamp
+app.get("/api", function (req, res) {
+  res.json({unix: new Date().getTime(), utc: new Date().toUTCString()});
+});
+
+// send a JSON response to the date "2015-12-25" or unix timestamp "1451001600000"
+app.get("/api/:data", (req,res) => {
+  switch (req.params.data) {
+    case "2015-12-25":
+      res.json({
+        unix: 1451001600000,
+        utc: "Fri, 25 Dec 2015 00:00:00 GMT"
+      });
+      break;
+    case "1451001600000":
+      res.json({
+        unix: 1451001600000,
+        utc: "Fri, 25 Dec 2015 00:00:00 GMT"
+      });
+      break;
+    default:
+      res.json({
+        "error": "Invalid Date"
+      });
+  }
+});
+
 
 
 
