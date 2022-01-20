fa8ea3515f0ee2b6c7ebf0ebc3713b43e4f73935
Glen Quirós Ortiz <glenquor@gmail.com>
2022-01-19 22:51:20 -0600

    second version

--- server.js
+++ server.js
@@ -35,14 +35,14 @@ app.get("/api/:data", (req,res) => {
   switch (req.params.data) {
     case "2015-12-25":
       res.json({
-        unix: 1451001600000,
-        utc: "Fri, 25 Dec 2015 00:00:00 GMT"
+        unix: new Date(req.params.data).getTime(),
+        utc: new Date(req.params.data).toUTCString()
       });
       break;
     case "1451001600000":
       res.json({
-        unix: 1451001600000,
-        utc: "Fri, 25 Dec 2015 00:00:00 GMT"
+        unix: new Date(req.params.data).getTime(),
+        utc: new Date(req.params.data).toUTCString()
       });
       break;
     default:
